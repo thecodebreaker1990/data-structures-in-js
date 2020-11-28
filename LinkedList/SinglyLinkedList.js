fc16753300d79d75dc0d858b32f7e265a742a586
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2020-11-28 19:40:05 +0530

    Implement shift & unshift in the linked list

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -14,7 +14,7 @@ class SinglyLinkedList {
     push(val) {
         const newNode = new Node(val);
         //Check if this node is the first element in the linkedList
-        if(this.length === 0) {
+        if(!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
@@ -32,7 +32,7 @@ class SinglyLinkedList {
         }
     }
     pop() {
-        if(this.length === 0) return undefined;
+        if(!this.head) return undefined;
         var current = this.head;
         var newTail = current;
         while(current.next !== null) {
@@ -48,6 +48,27 @@ class SinglyLinkedList {
         }
         return current;
     }
+    shift() {
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0) this.tail = null;
+        currentHead.next = null;
+        return currentHead;
+    }
+    unshift(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 const linkedList = new SinglyLinkedList();
@@ -57,5 +78,4 @@ linkedList.push("How");
 linkedList.push("Are");
 linkedList.push("You");
 linkedList.push("?");
-console.log(linkedList.pop());
-console.log(linkedList);
+console.log(linkedList.unshift("Folks"));

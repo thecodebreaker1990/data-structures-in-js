d7623f70dbc42f207a9fbae2da023424e3b7176f
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2020-11-28 14:16:23 +0530

    Implement POP method for Linked List

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -12,7 +12,7 @@ class SinglyLinkedList {
         this.length = 0;
     }
     push(val) {
-        const newNode = new newNode(val);
+        const newNode = new Node(val);
         //Check if this node is the first element in the linkedList
         if(this.length === 0) {
             this.head = newNode;
@@ -24,10 +24,38 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    traverse() {
+        var current = this.head;
+        while(current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+    pop() {
+        if(this.length === 0) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next !== null) {
+            newTail = current;
+            current = current.next;
+        }
+        newTail.next = null;
+        this.tail = newTail;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 
 const linkedList = new SinglyLinkedList();
-linkedList.push(5);
-linkedList.push(50);
-linkedList.push(100);
+linkedList.push("Hello");
+linkedList.push("There");
+linkedList.push("How");
+linkedList.push("Are");
+linkedList.push("You");
+linkedList.push("?");
+console.log(linkedList.pop());
 console.log(linkedList);
